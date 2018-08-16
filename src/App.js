5c2bd3d4f77fdd03d7278e5d93bab2659afff56f
emmanormana <emmanorman891@gmail.com>
2018-08-15 22:31:53 -0400

    Save before export to github

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
 
-const url = "https://simple-api-yeti.herokuapp.com";
-// const url = "http://localhost:3001";
+// const url = "https://simple-api-yeti.herokuapp.com";
+const url = "http://localhost:3001";
 
 class App extends Component {
 	state = {
-		email: "test@koidja.com"
+		email: "test@koidja.com",
+		name: "Julien",
+		message: "Hi I would like to have a new website!"
 	}
 
 	handleSubmit = e => {
 		e.preventDefault();
 
+		// Let's create the user object
+		const { email, name, message } = this.state;
+		const user = {
+			email,
+			name,
+			message
+		}
 		// Make the request to the email api
 		axios.post(`${ url }/email/new`, {
-			email: this.state.email
+			user: user
 		})
 			.then( (response) => {
 				console.log(response.data)
@@ -48,6 +57,31 @@ class App extends Component {
 								className="form-control"
 								required/>
 						</div>
+
+						<div className="form-group">
+							<label htmlFor="name">Name:</label>
+							<input
+								name="name"
+								value={ this.state.name }
+								onChange={ this.handleChange }
+								id="name"
+								type="text"
+								className="form-control"
+								required/>
+						</div>
+
+						<div className="form-group">
+							<label htmlFor="message">Message</label>
+							<textarea
+								name="message"
+								value={ this.state.message }
+								onChange={ this.handleChange }
+								id="message"
+								type="text"
+								className="form-control"
+								required/>
+						</div>
+
 						<button className="btn btn-primary">Submit</button>
 					</form>
 				</div>
