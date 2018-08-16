7d9348c0414dbaad08987d63086f39374902bf9a
emmanormana <emmanorman891@gmail.com>
2018-08-15 21:39:38 -0400

    Lets send email from online

--- src/App.js
+++ src/App.js
@@ -2,25 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
 
+const url = "https://simple-api-yeti.herokuapp.com";
+// const url = "http://localhost:3001";
+
 class App extends Component {
 	state = {
-		apiText: "Waiting..."
+		email: "test@koidja.com"
 	}
-  render() {
-		// Make a request for a user with a given ID
-		axios.get('https://simple-api-yeti.herokuapp.com/')
-		  .then( (response) => {
-		    this.setState({
-					apiText: response.data.text
-				});
-		  })
-		  .catch(function (error) {
-		    console.log(error);
-		  });
 
+	handleSubmit = e => {
+		e.preventDefault();
+
+		// Make the request to the email api
+		axios.post(`${ url }/email/new`, {
+			email: this.state.email
+		})
+			.then( (response) => {
+				console.log(response.data)
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+  render() {
     return (
-      <div className="App">
-        <h1>{ this.state.apiText }</h1>
+      <div className="App container">
+        <div className="col-lg-6 offset-lg-3">
+					<h1>Email Form API</h1>
+					<form onSubmit={ this.handleSubmit }>
+						<div className="form-group">
+							<label htmlFor="email">Email:</label>
+							<input
+								name="email"
+								value={ this.state.email }
+								onChange={ this.handleChange }
+								id="email"
+								type="email"
+								className="form-control"
+								required/>
+						</div>
+						<button className="btn btn-primary">Submit</button>
+					</form>
+				</div>
       </div>
     );
   }
